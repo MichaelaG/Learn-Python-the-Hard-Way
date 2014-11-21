72e9a7d862e9dac6fee0173202f521e36c4f0309
Michaela Gobron <michaelagobron@gmail.com>
2014-11-21 12:08:32 -0800

    Example 34
    Accessing elements in a list

---
+++ ex34.py
@@ -0,0 +1,27 @@
+# Example 34
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at 1 is at 1 and is a"
+print animals[1]
+
+print "The third animal is at 2 and is a"
+print animals[2]
+
+print "The first animal is at 0 and is a"
+print animals[1]
+
+print "The animal at 3 is at 3 and is a"
+print animals[3]
+
+print "The fifth animal is at 4 and is a"
+print animals[4]
+
+print "The animal at 2 is at 2 and is a"
+print animals[2]
+
+print "The sixth animal is at 5 and is a"
+print animals[5]
+
+print "The animal at 4 is at 4 and is a"
+print animals[4]
