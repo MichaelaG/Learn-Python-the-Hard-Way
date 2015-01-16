20413f9046c48797558afb7ff256612a42fd5773
Michaela Gobron <michaelagobron@gmail.com>
2015-01-16 15:58:44 -0800

    Exercise 52
    Making your own website game!
    Gothons On the Web!

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,24 +1,52 @@
 import web
+import map
 
 urls = (
-	'/hello', 'Index'
-	)
+	'/game', 'GameEngine',
+	'/', 'Index',
+)
 
 app = web.application(urls, globals())
 
+# littlr hack so that debug mode works with sessions
+if web.config.get('_session') is None:
+	store = web.session.DiskStore('sessions')
+	session = web.session.Session(app, store,
+									initializer = {'room': None})
+	web.config._session = session
+else:
+	session = web.config._session
+
 render = web.template.render('templates/', base = "layout")
 
-class Index:
+class Index(object):
 	def GET(self):
-		return render.hello_form()
+		# this is used to "setup" the session with starting values
+		session.room = map.START
+		web.seeother("/game")
+
+class GameEngine(object):
+	def GET(self):
+		if session.room:
+			return render.show_room(room = session.room)
+		#else:
+			# why is this here? do you need it?
+		#	return render.you_died()
 
 	def POST(self):
-		form = web.input(name = "Nobody", greet = "Hello") # get data from the browser using 'input'
-		#if form.greet:
-		greeting = "%s, %s" % (form.greet, form.name)
-		return render.index(greeting = greeting)
-		#else:
-		#	return "ERROR: greet is required"
+		form = web.input(action = "")
+
+		#there is a bug here, can you fix it?
+		if session.room and form.action:
+			next_room = session.room.go(form.action)
+			if next_room == None:
+				next_room = session.room.go('*')
+			elif next_room != None:
+				session.room = next_room
+			else:
+				session.room = None
+
+		web.seeother("/game")
 
 if __name__ == "__main__":
 	app.run()
